ad8f67fd69a464ea79603f980289f61377721814
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-09-30 17:49:55 +0600

    change logo and footer margin

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import Hero from "./Component/Banner/Hero";
+import Footer from "./Component/Footer/Footer";
 import Navber from "./Component/Navber";
 import Search from "./Component/Search/Search";
 import Table from "./Component/Table/Table";
-import TableHeader from "./Component/TableHeader/TableHeader";
 
 function App() {
   return (
@@ -13,9 +13,10 @@ function App() {
       <section className="py-24 lg:pt-[120px] lg:pb-28 bg-gray-900">
         <div className="container">
           <Search></Search>
-          
-
           <Table></Table>
+          <div className="mt-14">
+            <Footer></Footer>
+          </div>
         </div>
       </section>
     </>
--- src/Component/Navber.jsx
+++ src/Component/Navber.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/lws-logo-en.svg";
+
 export default function Navber() {
   return (
     <div>
@@ -6,7 +6,8 @@ export default function Navber() {
         <div className="container mx-auto flex items-center justify-between gap-x-6">
           {/* logo */}
           <a href="/">
-            <img className="h-[40px]" src={logo} alt="Lws" />
+            {/* <img className="h-[40px]" src={logo} alt="Lws" /> */}
+            <h2 className="text-3xl font-bold text-white">UniWise</h2>
           </a>
           {/* Logo Ends */}
           <a className="px-5 py-2 bg-[#172227] rounded-[44px] text-white" href="#">
